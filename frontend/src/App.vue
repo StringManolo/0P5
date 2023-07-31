09f39a4044813d74a26d2830e5b9e7996c18ebce
StringManolo <manuelvarelacaldas@gmail.com>
2023-07-31 15:57:45 +0000

    Basic SearchForm, SettingsMenu, App vue components and index.html

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <SearchForm />
+
+    <SettingsMenu />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import SearchForm from "./components/SearchForm.vue";
+import SettingsMenu from "./components/SettingsMenu.vue";
+
+@Component({
+  components:
+  {
+    SearchForm,
+    SettingsMenu,
+  },
+});
+
+export default class App extends Vue {}
+</script>
