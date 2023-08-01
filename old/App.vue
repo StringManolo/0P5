65909fe75b50cf59d890c0a6dd71f4fae820d5b1
StringManolo <manuelvarelacaldas@gmail.com>
2023-08-01 08:32:21 +0000

    Remplace VUE by React (starting from 0)

--- old/App.vue
+++ old/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
     <SearchForm />
-
     <SettingsMenu />
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { defineComponent} from 'vue';
 import SearchForm from "./components/SearchForm.vue";
 import SettingsMenu from "./components/SettingsMenu.vue";
 
-@Component({
-  components:
-  {
+export default defineComponent({
+  components: {
     SearchForm,
-    SettingsMenu,
+    SettingsMenu 
   },
 });
-
-export default class App extends Vue {}
 </script>
